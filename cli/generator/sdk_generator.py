893d6ea7bdc3843eb69df71c42572eeaa1e5b12e
ian luo <ianluo63@gmail.com>
2023-11-01 23:13:03 +0800

    start sdk generator

--- cli/generator/sdk_generator.py
+++ cli/generator/sdk_generator.py
@@ -1,8 +1,9 @@
 from ..configure.configure import Configure
 from .interface.file_exporter import FileExporter
 from .interface.content_generator import ContentGenerator
+from functools import reduce
 
-_TEMPLATE_FILE = 'templates/sdk.nix.template'
+_TEMPLATE_FILE_PYTHON = 'templates/sdk_python.template'
 _MARK_VERSION = '#VERSION#'
 _MARK_PACKAGES = '#PACKAGES#'
 
@@ -16,3 +17,14 @@ class SdkGenerator(ContentGenerator, FileExporter):
 
     def export(self) -> str:
         return ''
+
+    def generate_python(self, configure: Configure):
+       with open(_TEMPLATE_FILE_PYTHON, 'r') as f:
+           template = f.read()
+           template = template.replace(_MARK_VERSION, configure.sdk_version)
+           template = template.replace(_MARK_PACKAGES, self._render_packages(configure))
+
+           return template 
+
+    def _render_packages(self, configure: Configure):
+        return reduce(lambda last, next: f'{last}\n{next}', configure.sdk_packages_dev)
--- tests/test_nix_file_exportor.py
+++ tests/test_nix_file_exportor.py
@@ -5,5 +5,3 @@ from .fixtures import config
 class TestNixFileExportor:
     def test_export(self, config):
         exportor = NixFilesExportor(Configure(config))
-        pass
-
